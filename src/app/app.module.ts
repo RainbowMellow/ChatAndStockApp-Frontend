a3e6064b08cad8ace4bd93c81efd8aca2dc1be33
RainbowMellow <christina.nygaard@outlook.com>
2021-02-23 19:38:25 +0100

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {CommonModule} from '@angular/common';
 
 const config: SocketIoConfig = {url: 'http://localhost:4100', options: {}};
 
@@ -13,13 +14,14 @@ const config: SocketIoConfig = {url: 'http://localhost:4100', options: {}};
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SocketIoModule.forRoot(config),
-        FormsModule,
-        NgbModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    FormsModule,
+    NgbModule,
+    CommonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
